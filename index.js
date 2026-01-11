b220134fc7cce33a3b65eb97e2f07d5f01a1891a
BijoyDeep11 <bijoydeep007@gmail.com>
2026-01-11 19:45:40 +0530

    creating server

---
+++ index.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200,{'Content-Type': 'application/json'});
+//     res.end(JSON.stringify({ data: 'Hello, World!',}));
+// });
+
+// server.listen(8000);
+
+const server = http.createServer();
+
+server.on('request', (req, res) =>{
+    res.writeHead(200, {"Content-Type": "application/json" });
+    res.end(JSON.stringify({
+        data: "Hello World!"
+    }));
+});
+
+server.listen(8000);
