b27251fb1a5b4982d629494827ff2351cc7f9237
BijoyDeep11 <bijoydeep007@gmail.com>
2026-01-11 20:46:47 +0530

    routing practice

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
 const http = require('http');
 
-// const server = http.createServer((req, res) => {
-//     res.writeHead(200,{'Content-Type': 'application/json'});
-//     res.end(JSON.stringify({ data: 'Hello, World!',}));
+// const server = http.createServer();
+
+// server.on('request', (req, res) =>{
+//     res.writeHead(200, {"Content-Type": "application/json" });
+//     res.end(JSON.stringify({
+//         data: "Hello World!"
+//     }));
 // });
 
-// server.listen(8000);
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+    const { url, method } = req
 
-server.on('request', (req, res) =>{
-    res.writeHead(200, {"Content-Type": "application/json" });
-    res.end(JSON.stringify({
-        data: "Hello World!"
-    }));
+    res.setHeader("Content-Type", "text/plain")
+
+    if(url === "/" && method === "GET"){
+        res.statusCode = 200
+        res.end("HomePage")
+    }
+    else if(url === "/users" && method === "GET"){
+        res.statusCode = 200
+        res.end("Users List: ");
+    }
+    else if(url === "/login" && method === "POST"){
+        res.statusCode = 200
+        res.end("Login Here: ");
+    }
+    else if(url === "/login" && method === "GET"){
+        res.statusCode = 200
+        res.end("Login Here: ");
+    }
+    else{
+        res.statusCode = 404
+        res.end("404 Error Not found");
+    }
 });
 
-server.listen(8000);
+server.listen(8000, () => console.log("Server running on Port 8000"));
